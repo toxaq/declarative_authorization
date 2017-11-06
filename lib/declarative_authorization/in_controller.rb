1b41991da270060bab38b94e37fb4b79475d2ba0
Scott <toxaq@yahoo.com>
2017-11-07 11:07:46 +1300

    Update in_controller.rb

--- lib/declarative_authorization/in_controller.rb
+++ lib/declarative_authorization/in_controller.rb
@@ -324,7 +324,7 @@ module Authorization
         actions = args.flatten
 
         # prevent setting filter_access_filter multiple times
-        skip_before_action :filter_access_filter
+        skip_before_action :filter_access_filter, raise: false
         before_action :filter_access_filter
         
         filter_access_permissions.each do |perm|
